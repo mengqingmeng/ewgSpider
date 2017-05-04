786ecf53506d9b2fc30036c9e631a84b24cd8cc7
mengqingmeng <762577099@qq.com>
2017-05-04 11:40:45 +0800

    d

--- spider.py
+++ spider.py
@@ -9,7 +9,8 @@ import os
 
 # with open('/Users/mqm/Desktop/jtp/ewg/EWG待爬成分列表20170502.xlsx', 'r') as f:
 #     print(f.read())
-url ='http://www.ewg.org/skindeep/search.php'
+baseUrl = "http://www.ewg.org"
+firstUrl ='http://www.ewg.org/skindeep/search.php'
 
 
 #加载excel文件
@@ -18,25 +19,61 @@ wb = load_workbook("/Users/mqm/Desktop/jtp/ewg/EWG待爬成分列表20170502.xls
 #获取第一个Sheet
 sheet1 = wb.get_sheet_by_name('Sheet1')
 
+'''
+请求数据方法
+'''
+def requestData(url,params):
+    if params !=None:
+        req = request.Request(url)
+    else:
+        req = request.Request(url + '?' + params)
+    req.add_header('User-Agent',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/58.0.3029.81 Safari/537.36')
+    with request.urlopen(req) as f:
+        returnData = f.read().decode('utf-8')
+        return returnData
+
+'''
+获取详情信息
+'''
+def getDetail(url):
+    pass
+
+
 #遍历行
 for row in sheet1.rows:
     #遍历单元格
     for cell in row:
         cellValue = cell.value
         #如果是第一行跳过
-        if '待爬成分名称' in cellValue:
+        if 'name' in cellValue:
             continue
         #print("cell:",cell.value)
         queryParams = {
-            'query':cellValue
+            'query': cellValue,
+            'search_group': 'ingredients'
         }
+
         params = parse.urlencode(queryParams)
-        req = request.Request(url)
+        req = request.Request(firstUrl + '?' + params)
         req.add_header('User-Agent',
                        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) '
                        'Chrome/58.0.3029.81 Safari/537.36')
 
-        with request.urlopen(req,data = params.encode('utf-8')) as f:
-            returnData = f.read().decode('utf-8')
-            soup = BeautifulSoup(returnData)
-            print("data:",soup.prettify())
+        '''
+        1.取成分详细页面的链接
+        '''
+        returnData = requestData(firstUrl, queryParams)
+        soup = BeautifulSoup(returnData, 'lxml')
+        table_browse = soup.find(id='table-browse')
+        if table_browse != None:
+            # print("table_browse:" + str(table_browse))
+            centerTd = table_browse.find_all('tr')[1].find(align="center")
+            hrefs = centerTd.find_all('a')
+            detailUrl = baseUrl + hrefs[0]  # 获得，获取详细页链接
+
+            '''
+            获取详细页
+            '''
+            getDetail(detailUrl)
--- test.py
+++ test.py
@@ -9,33 +9,57 @@ import os
 # with open('/Users/mqm/Desktop/jtp/ewg/EWG待爬成分列表20170502.xlsx', 'r') as f:
 #     print(f.read())
 baseUrl = "http://www.ewg.org"
-url ='http://www.ewg.org/skindeep/search.php'
+firstUrl ='http://www.ewg.org/skindeep/search.php'
 
-#如果是第一行跳过
-cellValue = 'PLACENTAL ENZYMES'
+cellValue = 'PLACENTAL PROTEIN'
 
 queryParams = {
     'query':cellValue,
     'search_group':'ingredients'
 }
-params = parse.urlencode(queryParams)
-req = request.Request(url + '?' + params)
-req.add_header('User-Agent',
-               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) '
-               'Chrome/58.0.3029.81 Safari/537.36')
 
-with request.urlopen(req) as f:
-    returnData = f.read().decode('utf-8')
-    soup = BeautifulSoup(returnData,'lxml')
-    table_browse = soup.find(id='table-browse')
-    if table_browse != None:
-        print("table_browse:" + str(table_browse))
-        centerTd = table_browse.find_all('tr')[1].find(align="center")
-        hrefs = centerTd.find_all('a')
-        for href in hrefs:
-            print(baseUrl + href.get('href'))
-
-            # print(hrefs[0].get('href'))
+'''
+请求数据方法
+'''
+def requestData(url,params):
+    if params == None:
+        req = request.Request(url)
     else:
-        print("empty")
+        params = parse.urlencode(params)
+        req = request.Request(url + '?' + params)
+    req.add_header('User-Agent',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/58.0.3029.81 Safari/537.36')
+    with request.urlopen(req) as f:
+        returnData = f.read().decode('utf-8')
+        return returnData
+
+'''
+2.获取详情信息
+'''
+def getDetail(url):
+    returnData = requestData(url)
+    print(BeautifulSoup(returnData,'lxml'))
+
+'''
+1.根据成分名称，请求成分详情链接
+'''
+returnData = requestData(firstUrl,queryParams)
+soup = BeautifulSoup(returnData,'lxml')
+#print("soup:",soup)
+table_browse = soup.find(id='table-browse')
+#print("table_browse:" + str(table_browse))
+if table_browse != None:
+    #print("table_browse:" + str(table_browse))
+    centerTd = table_browse.find_all('tr')[1].find(align="center")
+    hrefs = centerTd.find_all('a')
+    detailUrl = baseUrl + hrefs[0]  #获得，获取详细页链接
+    print('detailURL:'+detailUrl)
+    # 获取详细页
+    getDetail(detailUrl)
+    #for href in hrefs:
+    #    print(baseUrl + href.get('href'))
+        # print(hrefs[0].get('href'))
+else:
+    print("empty")
 
